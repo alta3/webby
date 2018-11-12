46b529cf9d87e09e9e8dd6fa51ccda56d447e0bc
Stuart Feeser <sfeeser@gmail.com>
2018-11-11 22:37:13 -0500

    adding elements to the courselist dom

---
+++ deploy/html_menu_1/js/courselist.js
@@ -0,0 +1,16 @@
+const urlParams = new URLSearchParams(window.location.search);
+const myParam = urlParams.get('myParam');
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+var search = getParameterByName('search'); // "lorem"
+
+
